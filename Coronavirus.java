8fbc693788607142c7c322a51c938385e7114de4
Haobin <g.haobin@gmail.com>
2020-04-08 22:30:29 +0200

    1r commit cercle constructors

---
+++ Coronavirus.java
@@ -0,0 +1,36 @@
+public class Coronavirus{    
+	private float infeccio;
+	private float mortalidat;
+	private boolean sem;	
+	Coronavirus(float x, float y){
+		infeccio=x;
+		mortalidat=y;
+	}
+	public Coronavirus() {
+	}
+
+	@Override
+	public String toString() {
+        	return "Coronavirus{" + "infeccio=" + infeccio + ", mortalidat=" + mortalidat + '}';
+	}
+	@Override
+	public boolean equals(Object obj) {
+        	if (this == obj) {
+        	    	return true;
+        	}
+        	if (obj == null) {
+           		return false;
+        	}
+        	if (getClass() != obj.getClass()) {
+            		return false;
+        	}
+        	final Coronavirus other = (Coronavirus) obj;
+        	if (Float.floatToIntBits(this.infeccio) != Float.floatToIntBits(other.infeccio)) {
+            		return false;
+        	}
+        	if (Float.floatToIntBits(this.mortalidat) != Float.floatToIntBits(other.mortalidat)) {
+	            	return false;
+        	}
+        return true;
+    	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Primer fitxer al primer repositori de <Haobin Guo> sense coronavirus
